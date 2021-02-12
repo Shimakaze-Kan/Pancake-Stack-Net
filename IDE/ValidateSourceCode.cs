eaf02d97b8bc3dd32550911000bdf48156544b04
Shimakaze-Kan <wiktormd@outlook.com>
2021-02-12 15:47:17 +0100

    Fix if file ends with "Eat all of the pancakes!" instruction

--- IDE/ValidateSourceCode.cs
+++ IDE/ValidateSourceCode.cs
@@ -103,6 +103,9 @@ namespace IDE
             {
                 if (_sourceCode[i] == "Eat all of the pancakes!")
                     return true;
+
+                if (!string.IsNullOrEmpty(_sourceCode[i]))
+                    break;
             }
 
             return false;
