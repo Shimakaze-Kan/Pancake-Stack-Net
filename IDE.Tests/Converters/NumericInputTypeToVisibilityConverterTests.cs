07f2138cf60a8ff8d079d6912f1bc188cf9e2062
Shimakaze-Kan <wiktormd@outlook.com>
2021-02-17 22:29:12 +0100

    Add more unit tests

---
+++ IDE.Tests/Converters/NumericInputTypeToVisibilityConverterTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IDE.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IDE.Converters.Tests
+{
+    [TestClass()]
+    public class NumericInputTypeToVisibilityConverterTests
+    {
+        [TestMethod()]
+        public void Convert_ShouldReturnVisibilityVisible_WhenInputTypeIsNumeric()
+        {
+            NumericInputTypeToVisibilityConverter numericInputTypeToVisibilityConverter = new NumericInputTypeToVisibilityConverter();
+            var result = numericInputTypeToVisibilityConverter.Convert(new WaitingForInputEventArgs() { Type = InputType.Numeric }, null, null, null);
+
+            Assert.AreEqual(Visibility.Visible, result);
+        }
+
+        [TestMethod()]
+        public void Convert_ShouldReturnVisibilityHidden_WhenInputTypeIsNotNumeric()
+        {
+            NumericInputTypeToVisibilityConverter numericInputTypeToVisibilityConverter = new NumericInputTypeToVisibilityConverter();
+            var result = numericInputTypeToVisibilityConverter.Convert(new WaitingForInputEventArgs() { Type = InputType.Alphanumeric }, null, null, null);
+
+            Assert.AreEqual(Visibility.Hidden, result);
+        }
+
+        [TestMethod()]
+        public void Convert_ShouldReturnVisibilityHidden_WhenValueIsNull()
+        {
+            NumericInputTypeToVisibilityConverter numericInputTypeToVisibilityConverter = new NumericInputTypeToVisibilityConverter();
+            var result = numericInputTypeToVisibilityConverter.Convert(null, null, null, null);
+
+            Assert.AreEqual(Visibility.Hidden, result);
+        }
+
+        [TestMethod()]
+        public void ConvertBack_ShouldThrowNotImplementedException()
+        {
+            NumericInputTypeToVisibilityConverter numericInputTypeToVisibilityConverter = new NumericInputTypeToVisibilityConverter();
+
+            Assert.ThrowsException<NotImplementedException>(() => numericInputTypeToVisibilityConverter.ConvertBack(new object(), null, null, null));
+        }
+    }
+}
---
+++ IDE.Tests/Converters/WindowStateMinimizedToVisibleConverterTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IDE.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace IDE.Converters.Tests
+{
+    [TestClass()]
+    public class WindowStateMinimizedToVisibleConverterTests
+    {
+        [TestMethod()]
+        public void Convert_ShouldReturnVisibilityHidden_WhenWindowStateNormal()
+        {
+            WindowStateMaximizedToVisibleConverter windowStateMaximizedToVisibleConverter = new WindowStateMaximizedToVisibleConverter();
+            var result = windowStateMaximizedToVisibleConverter.Convert(WindowState.Normal, null, null, null);
+
+            Assert.AreEqual(Visibility.Hidden, result);
+        }
+
+        [TestMethod()]
+        public void Convert_ShouldReturnVisibilityVisible_WhenWindowStateNotNormal()
+        {
+            WindowStateMaximizedToVisibleConverter windowStateMaximizedToVisibleConverter = new WindowStateMaximizedToVisibleConverter();
+            var result = windowStateMaximizedToVisibleConverter.Convert(WindowState.Maximized, null, null, null);
+
+            Assert.AreEqual(Visibility.Visible, result);
+        }
+
+        [TestMethod()]
+        public void ConvertBack_ShouldThrowNotImplementedException()
+        {
+            WindowStateMinimizedToVisibleConverter windowStateMinimizedToVisibleConverter = new WindowStateMinimizedToVisibleConverter();
+
+            Assert.ThrowsException<NotImplementedException>(() => windowStateMinimizedToVisibleConverter.ConvertBack(new object(), null, null, null));
+        }
+    }
+}
